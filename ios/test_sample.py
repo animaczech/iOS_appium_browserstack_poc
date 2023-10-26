6fc4cc8f18406f896864a2608cf28dfe2c865cca
David Aldorf <david.aldorf@showmax.com>
2023-10-26 13:31:07 +0200

    fix example tests driver

--- ios/test_sample.py
+++ ios/test_sample.py
@@ -3,16 +3,16 @@ import Locators
 
 @pytest.mark.usefixtures('ios_driver')
 class TestSample_iOS:
-    def is_element_visible(self, driver, element_id):
+    def is_element_visible(self, ios_driver, element_id):
         try:
-            element = driver.find_element("id", element_id)
+            element = ios_driver.find_element("id", element_id)
             return element.is_displayed()
         except:
             return False
 
-    def test_home_screen_displayed(self, driver):
+    def test_home_screen_displayed(self, ios_driver):
         home_id = Locators.Locators.Home.screen
-        assert self.is_element_visible(driver, home_id)
+        assert self.is_element_visible(ios_driver, home_id)
 
-    def test_health_check(self, driver):
-        assert driver.find_element("id", "Showmax")
+    def test_health_check(self, ios_driver):
+        assert ios_driver.find_element("id", "Showmax")
