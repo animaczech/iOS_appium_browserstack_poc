0b68b744d643884d0e0d98625d6d9e73a3c87035
David Aldorf <david.aldorf@showmax.com>
2023-10-31 15:53:45 +0100

    add signed app and helper methods

--- ios/test_sample.py
+++ ios/test_sample.py
@@ -1,18 +1,42 @@
 import pytest
+from appium.webdriver.common.mobileby import MobileBy
 import Locators
 
 @pytest.mark.usefixtures('ios_driver')
 class TestSample_iOS:
-    def is_element_visible(self, ios_driver, element_id):
-        try:
-            element = ios_driver.find_element("id", element_id)
-            return element.is_displayed()
-        except:
-            return False
-
-    def test_home_screen_displayed(self, ios_driver):
-        home_id = Locators.Locators.Home.screen
-        assert self.is_element_visible(ios_driver, home_id)
-
     def test_health_check(self, ios_driver):
-        assert ios_driver.find_element("id", "Showmax")
+        close_button = Helpers_iOS().getElement("Close")
+        if not close_button.is_displayed():
+            close_button.click()
+        item = Helpers_iOS().scrollToElement("Home")
+        assert item.is_displayed()
+
+@pytest.mark.usefixtures('ios_driver')
+class Helpers_iOS:
+    def getElement(ios_driver, accessibilityID):
+        return ios_driver.find_element(MobileBy.ACCESSIBILITY_ID, accessibilityID)
+
+    def isElementVisible(ios_driver, accessibilityID):
+        element = ios_driver.getElement(MobileBy.ACCESSIBILITY_ID, accessibilityID)
+        return element.is_displayed()
+
+    def scrollToElement(self, ios_driver, accessibilityID, maxCount = 10):
+        # Find the element by accessibility ID
+        element = self.getElement(accessibilityID)
+        # Check if the element is visible on the screen
+        if not element.is_displayed():
+            # If not, use the driver's swipe method to perform a swipe gesture until the element is visible
+            # Get the screen size
+            size = ios_driver.get_window_size()
+            # Calculate the start and end coordinates for the swipe gesture
+            start_x = size["width"] // 2
+            start_y = size["height"] * 0.8
+            end_x = start_x
+            end_y = size["height"] * 0.2
+            # Perform the swipe gesture until the element is visible or a maximum of 10 times
+            count = 0
+            while not element.is_displayed() and count < maxCount:
+                ios_driver.swipe(start_x, start_y, end_x, end_y, duration=1000)
+                count += 1
+        # Return the element
+        return element
