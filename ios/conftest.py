fa2c5142c0db48a53fe8fd4f9991835da61a3bff
David Aldorf <david.aldorf@showmax.com>
2023-11-02 10:54:48 +0100

    upload new ipa and change bs yml

--- ios/conftest.py
+++ ios/conftest.py
@@ -30,25 +30,25 @@ def appium_options_ios():
     return options
 
 ## Remote BrowserStack - uncomment this if you want to run tests on BS
-# @pytest.fixture(scope='function')
-# def ios_driver(request, session_capabilities):
-#     remoteURL = "https://hub.browserstack.com/wd/hub"
-#     driver = webdriver.Remote(remoteURL, session_capabilities)
-#     request.instance.driver = driver
-#     request.node._driver = driver
-#     yield driver
-#     driver.quit()
+@pytest.fixture(scope='function')
+def ios_driver(request, session_capabilities):
+    remoteURL = "https://hub.browserstack.com/wd/hub"
+    driver = webdriver.Remote(remoteURL, session_capabilities)
+    request.instance.driver = driver
+    request.node._driver = driver
+    yield driver
+    driver.quit()
 
 # Local Simulator - uncomment this if you want to run tests on iOS Simulator
-@pytest.fixture(scope='function')
-def ios_driver():
-    appium_service = AppiumService()
-    appium_service.start()
-
-    # configure port on localHost to match running Appium server e.g. 127.0.0.1/4723
-    driver = webdriver.Remote(command_executor='http://localhost:4723', desired_capabilities=appium_options_ios())
-
-    yield driver
-
-    driver.quit()
-    appium_service.stop()
+# @pytest.fixture(scope='function')
+# def ios_driver():
+#     appium_service = AppiumService()
+#     appium_service.start()
+#
+#     # configure port on localHost to match running Appium server e.g. 127.0.0.1/4723
+#     driver = webdriver.Remote(command_executor='http://localhost:4723', desired_capabilities=appium_options_ios())
+#
+#     yield driver
+#
+#     driver.quit()
+#     appium_service.stop()
