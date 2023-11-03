6782b7dbdc8518da5a05585ecd9fe021f672023b
Martin Stachon <stachon@gmail.com>
2023-11-03 09:50:13 +0100

    fix scrolling, new ipa, autoaccept notifications popup

--- ios/test_sample.py
+++ ios/test_sample.py
@@ -5,13 +5,17 @@ import Locators
 @pytest.mark.usefixtures('ios_driver')
 class TestSample_iOS:
     def test_health_check(self, ios_driver):
+        ios_driver.find_element(MobileBy.NAME, "Allow").click()
         item = Helpers_iOS().scrollToElement(ios_driver, "home_row")
         assert item.is_displayed()
 
 @pytest.mark.usefixtures('ios_driver')
 class Helpers_iOS:
     def getElement(self, ios_driver, accessibilityID):
-        return ios_driver.find_element(MobileBy.ACCESSIBILITY_ID, accessibilityID)
+        try:
+            return ios_driver.find_element(MobileBy.ACCESSIBILITY_ID, accessibilityID)
+        except:
+            return None
 
     def isElementVisible(self, ios_driver, accessibilityID):
         element = self.getElement(ios_driver, accessibilityID)
@@ -21,7 +25,7 @@ class Helpers_iOS:
         # Find the element by accessibility ID
         element = self.getElement(ios_driver, accessibilityID)
         # Check if the element is visible on the screen
-        if not element.is_displayed():
+        if element is None or not element.is_displayed():
             # If not, use the driver's swipe method to perform a swipe gesture until the element is visible
             # Get the screen size
             size = ios_driver.get_window_size()
@@ -32,7 +36,10 @@ class Helpers_iOS:
             end_y = size["height"] * 0.2
             # Perform the swipe gesture until the element is visible or a maximum of 10 times
             count = 0
-            while not element.is_displayed() and count < maxCount:
+            while True:
+                element = self.getElement(ios_driver, accessibilityID)
+                if element is not None and element.is_displayed() or count > maxCount:
+                    return element
                 ios_driver.swipe(start_x, start_y, end_x, end_y, duration=1000)
                 count += 1
         # Return the element
